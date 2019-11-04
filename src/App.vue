874656d89c12f6c026fb36e20caf0a87410999c3
Michael Leonard <michael.leonard@cqlcorp.local>
2019-11-04 16:57:32 -0500

    Add Todo component with some interactivity

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <button @click="addTodo">Add Todo</button>
+    </div>
+    <Todo v-for="todo in todos" :key="todo.id" :task="todo.task" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Todo from "./components/Todo";
 
 export default {
   name: "app",
+  data: () => ({
+    todos: [
+      {
+        id: 1,
+        task: "Eat"
+      },
+      {
+        id: 2,
+        task: "Poop"
+      }
+    ],
+    latestId: 2
+  }),
   components: {
-    HelloWorld
+    Todo
+  },
+  methods: {
+    addTodo: function() {
+      this.latestId++;
+      this.todos.push({ id: this.latestId, task: "newThing" });
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="todo">{{ task }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: {
+    task: String
+  }
+};
+</script>
